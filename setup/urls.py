b1cf6b086e8fb926954bc3fe2d811a4a4e29f456
gilberto19 <gilberto@smartinnovation.com.br>
2024-07-15 23:27:42 -0300

    feat: creating simple crud with django

--- setup/urls.py
+++ setup/urls.py
@@ -1,22 +1,19 @@
-"""
-URL configuration for setup project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
+from todos.views import (
+    TodoListView,
+    TodoCreateView,
+    TodoUpdateView,
+    TodoDeleteView,
+    TodoCompleteView,
+)
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("todos", TodoListView.as_view(), name="todo_list"),
+    path("todo", TodoCreateView.as_view(), name="todo_create"),
+    path("todo/<int:pk>", TodoUpdateView.as_view(), name="todo_update"),
+    path("todo/<int:pk>/delete", TodoDeleteView.as_view(), name="todo_delete"),
+    path("todo/<int:pk>/complete", TodoCompleteView.as_view(), name="todo_complete"),
 ]
--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=False, blank=False)
+    finished_at = models.DateTimeField(null=True)
+
+    class Meta:
+        ordering = ["deadline"]
+
+    def mark_as_complete(self):
+        if not self.finished_at:
+            self.finished_at = date.today()
+            self.save()
